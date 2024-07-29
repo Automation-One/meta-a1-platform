b52f916796170d70cb8cd25ebbea3d87d78fb545
Boerge Struempfel <boerge.struempfel@gmail.com>
2024-07-29 02:37:29 +0200

    u-boot-mainline: Change boot.cmd to auto-attach a ubifs to the spi-nand

--- recipes-bsp/u-boot-mainline/files/boot.cmd
+++ recipes-bsp/u-boot-mainline/files/boot.cmd
@@ -1,3 +1,3 @@
 load mmc 0 $loadaddr /boot/fitImage
-setenv bootargs "console=ttymxc3,115200 root=/dev/mmcblk0p1 rootwait ro"
+setenv bootargs "console=ttymxc3,115200 root=/dev/mmcblk0p1 rootwait ro  ubi.mtd=0"
 bootm $loadaddr
